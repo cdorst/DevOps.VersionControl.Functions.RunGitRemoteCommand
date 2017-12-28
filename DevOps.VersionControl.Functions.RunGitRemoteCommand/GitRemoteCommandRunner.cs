4fa929c723f31e5f17130f52c0216e4451064677
Christopher Dorst <chris@dorst.email>
2017-12-28 00:57:19 -0600

    Add GitRemoteCommandRunner

---
+++ DevOps.VersionControl.Functions.RunGitRemoteCommand/GitRemoteCommandRunner.cs
@@ -0,0 +1,14 @@
+﻿using static DevOps.VersionControl.Functions.RunGitCommand.GitCommandRunner;
+
+namespace DevOps.VersionControl.Functions.RunGitRemoteCommand
+{
+    public static class GitRemoteCommandRunner
+    {
+        private const string remote = nameof(remote);
+
+        public static void Remote(string directory, params string[] arguments)
+            => Git(directory,
+                command: remote,
+                arguments);
+    }
+}
